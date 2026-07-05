ade5ac4c845c088eac28d4c85caf12b5c7e577b1
1MichD1 <wrx95828@student.wroclaw.merito.pl>
2026-07-05 18:03:52 +0200

    "Test sprawdzający poprawność działania skonfigurowanego CircleCI"

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -35,3 +35,5 @@ def plain_text_upper_case(msg, imie):
 
 def plain_text_lower_case(msg, imie):
     return plain_text(msg.lower(), imie.lower())
+
+#komentarz do sprawdzenia działania poprawności działania CircleCI
